131c9dc158b0ba3c1da3e0a31bfa1f9f6ba73c23
wiciuberger <145281618+wiciuberger@users.noreply.github.com>
2024-09-18 08:06:12 +0200

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,13 @@
+const buttonRed = document.querySelector('.red-button') 
+const buttonGreen = document.querySelector('.green-button')
+const buttonOrange = document.querySelector('.orange-button')
+const buttonPink = document.querySelector('.pink-button')
+const buttonPurple = document.querySelector('.purple-button')
+const buttonBlack = document.querySelector('.black-button')
+const buttonBlue = document.querySelector('.blue-button')
+const buttonBeige = document.querySelector('.beige-button')
+const buttonBrown = document.querySelector('.brown-button')
+
+
+const body = document.body
+
---
+++ main.js
@@ -0,0 +1,59 @@
+buttonRed.addEventListener('click', () => {
+    changeColor('red');
+});
+
+buttonGreen.addEventListener('click', ()=>{
+    changeColor('green')
+})
+
+buttonOrange.addEventListener('click', ()=>{
+    changeColor('orange')
+})
+
+buttonPink.addEventListener('click', ()=>{
+    changeColor('pink')
+})
+
+buttonPurple.addEventListener('click', () => {
+    changeColor('purple')
+})
+
+buttonBlack.addEventListener('click', () => {
+    changeColor('black')
+})
+
+buttonBlue.addEventListener('click', () => {
+    changeColor('blue')
+})
+
+buttonBeige.addEventListener('click', () => {
+    changeColor('beige')
+})
+
+buttonBrown.addEventListener('click', () => {
+    changeColor('brown')
+})
+
+buttonBlue.addEventListener('click', () => {
+    changeColor('blue')
+})
+
+
+
+
+// buttonPink.addEventListener
+
+// addEventListener to metoda, 
+// która dodaje nasłuchiwacza zdarzeń 
+// do elementu. W tym przypadku nasłuchujemy 
+// na zdarzenie typu "click". Oznacza to, że 
+// kiedy użytkownik kliknie przycisk o klasie 
+// .pink-button, zostanie uruchomiona określona funkcja.
+
+// () => { ... }:
+
+// To jest funkcja strzałkowa (arrow function) w 
+// JavaScript, która zostanie wywołana po kliknięciu na przycisk. 
+// Funkcje strzałkowe to skrócony zapis funkcji anonimowych. 
+// W tym przypadku, po kliknięciu, funkcja zostanie wywołana.
+// changeColor('pink'):
